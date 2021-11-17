31cdc4a69d02a8df7161c4c63c96e8bfa4fe5cac
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-11-17 13:46:34 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+
+export default {
+  components: {
+    Navbar,
+    Hero,
+  },
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Navbar />
+  <Hero />
 </template>
 
 <style>
@@ -14,8 +20,6 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,10 @@
 <template>
-  <header class="flex justify-between mx-7">
-    <div class="logo w-16">
+  <header>
+    <div class="logo">
       <img src="../assets/img/logo.png" alt="logo_img" />
     </div>
-    <ul class="flex items-center justify-end">
-      <li
-        class="
-          menu
-          active
-          hover:bg-greyblueHover
-          px-6
-          py-2
-          rounded-full
-          transition
-          duration-300
-        "
-      >
+    <ul>
+      <li class="menu active">
         <a href="#">Home</a>
       </li>
       <li
@@ -36,7 +25,7 @@
       </li>
       <li class="mx-5">
         <a href="#">
-          <img src="../assets/img/search.png" class="w-5" alt="" />
+          <img src="../assets/img/search.png" class="search" alt="" />
         </a>
       </li>
     </ul>
@@ -47,14 +36,41 @@
 export default {};
 </script>
 
-<style>
-.active {
-  background-color: #e0e2e2;
-  border-radius: 30px;
-  padding: 1.5rem 0.5rem;
-}
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 1.75rem;
 
-li.menu {
-  margin: 0 10px;
+  .logo {
+    img {
+      width: 4rem;
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    li {
+      list-style: none;
+      a {
+        text-decoration: none;
+      }
+      .active {
+        background-color: #e0e2e2;
+        border-radius: 30px;
+        padding: 1.5rem 0.5rem;
+      }
+
+      .menu {
+        margin: 0 10px;
+      }
+    }
+
+    .search {
+      width: 1.25rem;
+    }
+  }
 }
 </style>
